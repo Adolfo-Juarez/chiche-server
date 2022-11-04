621190da8967ce5ffb4ec7af807defc91b36bc59
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2022-11-04 01:58:45 -0600

    refactor: delete unused entities

--- src/main/java/chiche/server/cookie/services/interfaces/ICookieService.java
+++
@@ -1,8 +0,0 @@
-package chiche.server.cookie.services.interfaces;
-
-public interface ICookieService {
-    // Por Hacer:
-    // - Genera las interfaces de lo métodos para los servicios
-
-    // Recuerda que mínimo deberán haber 4 métodos (del protocolo HTTP)
-}
--- src/main/java/chiche/server/pancake/services/interfaces/IPancakeService.java
+++
@@ -1,8 +0,0 @@
-package chiche.server.pancake.services.interfaces;
-
-public interface IPancakeService {
-    // Por Hacer:
-    // - Genera las interfaces de lo métodos para los servicios
-
-    // Recuerda que mínimo deberán haber 4 métodos (del protocolo HTTP)
-}
