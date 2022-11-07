704afa1a355b88182889f5f6410db7cec9ed79e1
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2022-11-06 22:29:09 -0600

    hotfix: allow cross origin request

--- src/main/java/chiche/server/order/controllers/OrderController.java
+++ src/main/java/chiche/server/order/controllers/OrderController.java
@@ -3,6 +3,7 @@ package chiche.server.order.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import chiche.server.order.services.interfaces.IOrderService;
 
 @RestController
 @RequestMapping("order")
+@CrossOrigin
 public class OrderController {
 
     @Autowired
