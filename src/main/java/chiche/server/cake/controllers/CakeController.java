70f24b7d193e4bde51db66bf4bcd4fc142525983
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2022-11-07 00:26:48 -0600

    update: CORS as global conf

--- src/main/java/chiche/server/cake/controllers/CakeController.java
+++ src/main/java/chiche/server/cake/controllers/CakeController.java
@@ -3,7 +3,6 @@ package chiche.server.cake.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import chiche.server.cake.services.interfaces.ICakeService;
 
 @RestController
 @RequestMapping("cake")
-@CrossOrigin
 public class CakeController {
 
     @Autowired
--- src/main/java/chiche/server/user/controllers/UserController.java
+++ src/main/java/chiche/server/user/controllers/UserController.java
@@ -3,7 +3,6 @@ package chiche.server.user.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,6 @@ import chiche.server.user.controllers.dtos.responses.LoginUserResponse;
 import chiche.server.user.services.interfaces.IUserService;
 
 @RestController
-@CrossOrigin
 @RequestMapping("user")
 public class UserController {
 
